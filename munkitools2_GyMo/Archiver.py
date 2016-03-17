99af60809f81923e8e4c28b82c66b29eadbe3fac
Philippe Rochat <fake236@hispeed.ch>
2016-03-17 23:51:29 +0100

    Some cleaning

--- munkitools2_GyMo/Archiver.py
+++ munkitools2_GyMo/Archiver.py
@@ -3,6 +3,8 @@
 # Based on Per Olofsson Unarchiver
 # Copyright 2016 Philippe Rochat
 #
+# But tar, all other archiver are INCOMPLETE !!!
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
